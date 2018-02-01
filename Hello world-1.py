a1462b2836c2d891829f00ca861721e9db910433
sebastianflores950@yahoo.com <sebastianflores950@yahoo.com>
2018-01-31 20:54:39 -0700

    message

---
+++ Hello world-1.py
@@ -0,0 +1,9 @@
+#Sebastian Flores and exercise 1
+print("Hello world")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'll much rather you 'not'")
+print('I "said" od not touch this.')
+print("Te amooo X 25")
